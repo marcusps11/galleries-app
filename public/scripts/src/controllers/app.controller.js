6549979d684b9dec06fdb3006806569ae32c8d27
marcus petty-saphon <mpetty-saphon@vccp.com>
2017-01-25 12:09:42 +0000

    adding marker functionality to map

--- public/scripts/src/controllers/app.controller.js
+++ public/scripts/src/controllers/app.controller.js
@@ -2,27 +2,37 @@ app.controller('appController', appController);
 
 appController.$inject = ['$scope', 'artService', 'NgMap'];
 
-    function appController($scope, artService, NgMap) {
-        $scope.message = 'hello';
+function appController($scope, artService, NgMap) {
+    $scope.message = 'hello';
+    $scope.position = [];
 
-        function init() {
-            artService.getArtFromGoogleSpreadSheet().then(onGalleriesLoad);
-            getGoogleMap();
-        }
-
-        function onGalleriesLoad(galleries) {
-            $scope.galleries = galleries;
-    
-        }
-
-        function getGoogleMap() {
-             NgMap.getMap().then(function (map) {
-                 console.log(map)
-            console.log(map.getCenter());
-            console.log('markers', map.markers);
-            console.log('shapes', map.shapes);
-        });
-        }
-        init();
+    function init() {
+        artService.getArtFromGoogleSpreadSheet().then(onGalleriesLoad);
+        getGoogleMap();
+        getLatLng();
     }
+
+    function onGalleriesLoad(galleries) {
+        $scope.galleries = galleries;
+
+    }
+
+    function getGoogleMap() {
+        NgMap.getMap().then(function (map) {
+
+        });
+    }
+
+    function getLatLng() {
+        var lat = 43.6600000;
+        var lng = -79.4103000;
+
+        $scope.position.push({
+            lat: lat,
+            lng: lng
+        })
+        console.log($scope.position)
+    }
+    init();
+}
 
